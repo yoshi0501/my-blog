2b992bf0ab9191532e2a1a3b39af6dd1fca1225d
yoshi0501 <63772617+yoshi0501@users.noreply.github.com>
2020-08-11 16:29:03 +0900

    Revert "Add weight"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200811065419) do
+ActiveRecord::Schema.define(version: 20200811015254) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20200811065419) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
-    t.integer  "weight"
   end
 
 end
